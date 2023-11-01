aa2c549e8fb2a9da3810971b792c9db2394ba79f
Vilijarvinen <vili.ja@pp.inet.fi>
2023-11-01 20:13:46 +0200

    backend and frontend connection established

--- solinavintage/src/App.js
+++ solinavintage/src/App.js
@@ -1,10 +1,20 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+/*   const [testMessage, setTestMessage] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:5000/testi")
+    .then((res) => res.json())
+    .then((data) => setTestMessage(data.test));
+  }, []); */
+
   return (
     <div className="App">
+      {/* <h1>{testMessage}</h1> */}
        <Header />
        <Footer />
     </div>
