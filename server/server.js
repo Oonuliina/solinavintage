74cb51407326791ba07dc3ca0e70d40574db57be
Vilijarvinen <vili.ja@pp.inet.fi>
2023-11-01 19:46:48 +0200

    backend stuff

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-mongoose.connect('mongodb+srv://vilijarvinen97:Q4sfKGTxHnBoiKDS@solinavintage.albjgbc.mongodb.net/').then(() => console.log('Connected to database'));
+app.use(express.json());
+
+mongoose.connect(process.env.URIKEY).then(() => console.log('Connected to database'));
+
+app.get('/testi', (req, res) => {
+    res.json({test: "Serveri toimii"});
+});
 
 app.listen(PORT, () => {
     console.log("Server is up and running at port " + PORT);
