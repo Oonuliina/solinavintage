36e7ae6aafc4a7d19db1807a1f27d98a304420a4
Vilijarvinen <vili.ja@pp.inet.fi>
2023-11-17 10:12:04 +0200

    sessions

--- server/server.js
+++ server/server.js
@@ -4,11 +4,30 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 const app = express();
+const sessions = require("express-session");
+const cookies = require("cookie-parser");
 
 app.use(cors());
 app.use(express.json());
+app.use(cookies());
+
+const aWeek = 1000 * 60 * 60 * 24 * 7;
+
+app.use(sessions({
+    secret: process.env.SESSIONSECRET,
+    saveUninitialized: true,
+    cookie: {maxAge: aWeek},
+    resave: false
+}));
+
+var session;
 
 mongoose.connect(process.env.URIKEY).then(() => console.log('Connected to database'));
+
+app.all('*', (req, res) => {
+    session = req.session;
+    console.log(session);
+});
 
 app.get('/testi', (req, res) => {
     res.json({test: "Serveri toimii"});
