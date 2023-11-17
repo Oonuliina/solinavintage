cfd047988bbbd29f2c633fa32a4fb381a52a6510
Vilijarvinen <vili.ja@pp.inet.fi>
2023-11-17 12:10:39 +0200

    asd

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const sessions = require("express-session");
 const cookies = require("cookie-parser");
+const {v4: uuidv4} = require("uuid");
 
 app.use(cors());
 app.use(express.json());
@@ -14,23 +15,21 @@ app.use(cookies());
 const aWeek = 1000 * 60 * 60 * 24 * 7;
 
 app.use(sessions({
+    name: "SolinaCookie",
     secret: process.env.SESSIONSECRET,
     saveUninitialized: true,
-    cookie: {maxAge: aWeek},
-    resave: false
+    cookie: {secure: false, maxAge: aWeek},
+    resave: false,
 }));
 
 var session;
 
 mongoose.connect(process.env.URIKEY).then(() => console.log('Connected to database'));
 
-app.all('*', (req, res) => {
+app.get('/getcart', (req, res) => {
     session = req.session;
-    console.log(session);
-});
-
-app.get('/testi', (req, res) => {
-    res.json({test: "Serveri toimii"});
+    console.log(session)
+    res.json({product: "tuote1"});
 });
 
 app.listen(PORT, () => {
